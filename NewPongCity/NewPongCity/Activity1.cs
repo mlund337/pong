6041fb17621d0e6c954b936d01d92d6eedd2b449
Adalaen <hunterangelovic@gmail.com>
2019-04-02 13:19:42 -0600

    Always Landscape

--- NewPongCity/NewPongCity/Activity1.cs
+++ NewPongCity/NewPongCity/Activity1.cs
@@ -9,9 +9,8 @@ namespace NewPongCity
         , MainLauncher = true
         , Icon = "@drawable/icon"
         , Theme = "@style/Theme.Splash"
-        , AlwaysRetainTaskState = true
         , LaunchMode = Android.Content.PM.LaunchMode.SingleInstance
-        , ScreenOrientation = ScreenOrientation.FullUser
+        , ScreenOrientation = ScreenOrientation.Landscape
         , ConfigurationChanges = ConfigChanges.Orientation | ConfigChanges.Keyboard | ConfigChanges.KeyboardHidden | ConfigChanges.ScreenSize | ConfigChanges.ScreenLayout)]
     public class Activity1 : Microsoft.Xna.Framework.AndroidGameActivity
     {
